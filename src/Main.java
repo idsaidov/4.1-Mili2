8598b47d99db51b8aeb5fe99cf3fa105d112cb1f
idsaidov <id.saidov@yandex.ru>
2022-08-07 20:30:19 +0300

    Pravka

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 1000;
+        int price = 10_000;
         int miles = service.calculate(price);
         System.out.println("За каждые 20 рублей, потраченные на билет, вам начисляется 1 миля. За эту покупку вам начислено " + miles +" бонус(ов)!");
     }
